7d08263f0b453a82d34e17cc391c2eec18f08e7b
Nana Dev <chukafe0401@gmail.com>
2026-08-01 17:12:04 +0700

    Improve authentication dependency

--- src/app/auth/dependencies.py
+++ src/app/auth/dependencies.py
@@ -1,7 +1,10 @@
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/login/"
+)
 
 
 def get_current_token(
