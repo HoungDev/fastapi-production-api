809ecc89ee961ce20bb1d590df9b4cb7fce915bd
Nana Dev <chukafe0401@gmail.com>
2026-08-01 16:03:30 +0700

    Add login endpoint tests

---
+++ tests/test_login.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+client = TestClient(app)
+
+
+def test_login():
+    response = client.post(
+        "/login/",
+        data={
+            "username": "houngdev",
+            "password": "secret123",
+        },
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert "access_token" in data
+    assert data["token_type"] == "bearer"
