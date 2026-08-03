13de89646ca091d0b043ba0fb7447df953c09eb4
Nana Dev <chukafe0401@gmail.com>
2026-08-03 16:14:30 +0700

    Improve JWT token dependency handling

--- src/app/auth/get_current_user.py
+++ src/app/auth/get_current_user.py
@@ -1,8 +1,11 @@
+from fastapi import Depends
+
 from app.auth.decode_token import decode_token
+from app.auth.oauth2 import oauth2_scheme
 
 
 def get_user_from_token(
-    token: str,
+    token: str = Depends(oauth2_scheme),
 ):
     payload = decode_token(token)
 
