840fd9e26f53e9b19b0d40576697bffb6d622ae8
Nana Dev <chukafe0401@gmail.com>
2026-08-03 15:49:05 +0700

    Implement login authentication with JWT

--- src/app/auth/login.py
+++ src/app/auth/login.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
 
 from app.auth.jwt import create_access_token
+from app.auth.security import verify_password
+from app.db.dependency import get_db
+from app.models.user import User
 from app.schemas import Token
+
 
 router = APIRouter(
     prefix="/login",
@@ -13,9 +18,29 @@ router = APIRouter(
 @router.post("/", response_model=Token)
 def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
 ):
+    user = db.query(User).filter(
+        User.username == form_data.username
+    ).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password",
+        )
+
+    if not verify_password(
+        form_data.password,
+        user.password,
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password",
+        )
+
     access_token = create_access_token(
-        {"sub": form_data.username}
+        {"sub": user.username}
     )
 
     return Token(
